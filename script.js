a26bd973a08ad36b8a470104ebfbac667c8f9d3c
Bruno S. T. Hofman <brunohofman2004@gmail.com>
2022-07-26 09:16:50 -0300

    meio que funciona

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-var min = 25;
-var sec = 60;
-var startB = document.getElementById("startB");
+var min = 2;
+var sec = 0;
 
 function startTimer(){
-
-
-    window.setInterval(countdown(), 1000);
+    countdown()
+    document.getElementById('startB').disabled = true;
+    
 }
 
 function countdown(){
-    while (min > 0){
+    setInterval(function(){
         sec -= 1;
-        document.getElementById("minutes").innerHTML = min;
-        document.getElementById("seconds").innerHTML = sec;
-    }
+        if (sec <= 0){
+            min -= 1;
+            sec = 60
+        }
+        if (min < 0){
+            countdownFinished()
+            
+        }
+        document.getElementById('minutes').innerHTML = min;
+        document.getElementById('seconds').innerHTML = sec;
+    }, 1000);   
+}
 
-    
+function countdownFinished(){
+    min = '--';
+    sec = '--';
 }
